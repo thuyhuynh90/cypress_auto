6c969a8615c65fb47b5f397b7ec310226f95d7c7
Thuy Huynh Thi Thanh <thuy.huynh>
2024-07-05 12:36:59 +0700

    add new file

---
+++ cypress/e2e/testfile.spec.js
@@ -0,0 +1,8 @@
+describe('Login Page tests', () => {
+    beforeEach('Open main page', () => {
+        cy.visit('./');
+    })
+    it('Homepage should be visible', () => {
+        cy.url()
+    })
+})
